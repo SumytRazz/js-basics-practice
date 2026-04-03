233d193c9cbf25071f372641b690a03bbe8b1b4c
SumytRazz <sumitrajthakur@gmail.com>
2026-04-03 17:26:30 +0530

    Added data types practice

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let age = 20;
 console.log("Name:", name);
 console.log("Age:", age);
 
+console.log("-------------")
 // changing value
 age = 21;
 console.log("Updated Age:", age);
@@ -18,3 +19,41 @@ console.log("Country:", country);
 // var example (avoid in real projects)
 var city = "Kathmandu";
 console.log("City:", city);
+
+
+console.log("-------------")
+// ===== DATA TYPES PRACTICE =====
+
+let name2 = "Sumit"; // string
+let age2 = 20; // number
+let isStudent = true // boolean
+let x; //undefined
+let y = null; // null
+
+console.log(typeof name2);
+console.log(typeof age2);
+console.log(typeof isStudent);
+console.log(typeof x);
+console.log(typeof y);
+
+
+console.log("-------------")
+// Practice
+
+let marks = 20;
+let isPassed = true;
+let a;
+
+console.log(typeof name2);
+console.log(typeof marks);
+console.log(typeof isPassed);
+console.log(typeof a);
+
+console.log("-------------")
+// Mini Task
+
+console.log(`My name is ${name} (${typeof name}).`);
+console.log(`My age is ${age} (${typeof age}).`);
+console.log(`I am a student: ${isStudent} (${typeof isStudent}).`)
+
+
