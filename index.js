f64a9144c37bb813f8cc923bbe5b340d6be14e63
SumytRazz <sumitrajthakur@gmail.com>
2026-04-03 17:03:41 +0530

    Added variable practice

---
+++ index.js
@@ -0,0 +1,20 @@
+// ===== VARIABLES PRACTICE =====
+
+// let example
+let name = "Sumit";
+let age = 20;
+
+console.log("Name:", name);
+console.log("Age:", age);
+
+// changing value
+age = 21;
+console.log("Updated Age:", age);
+
+// const example
+const country = "Nepal";
+console.log("Country:", country);
+
+// var example (avoid in real projects)
+var city = "Kathmandu";
+console.log("City:", city);
